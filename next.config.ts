a437d6ce4ef623d2715b95cec308c440d328a53a
zhenyu zhou <memorybankzzy@gmail.com>
2025-06-29 02:14:01 +0800

    revert build config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/findyourchina",
-  output: "export",  
   images: {
     remotePatterns: [
       {
